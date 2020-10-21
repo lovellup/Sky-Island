2f60163b19046883f1e4fcc97425248d4a40ead7
Ian Lovell <ian.lovell96@gmail.com>
2020-10-21 06:43:54 -0500

    Flip Sprite on moveLeft
    also broke out the move left and right ino methods

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -9,29 +9,46 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rigidBody2d;
     private Harpoon harpoon;
+    private SpriteRenderer spriteRenderer;
 
-    private void Awake() {
+    private void Awake()
+    {
         rigidBody2d = GetComponent<Rigidbody2D>();
         harpoon = GetComponent<Harpoon>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
-    
+
     private void Update()
     {
         HandleMovement();
     }
 
-    private void HandleMovement() {
+    private void HandleMovement()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             harpoon.AimHarpoon(Utils.GetMouseWorldPosition());
         }
-        if(Input.GetKey(KeyCode.A)) {
-            rigidBody2d.velocity += Vector2.left * movementSpeed * Time.deltaTime;
-            harpoon.CancelHarpoon();
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveLeft();
         }
-        if(Input.GetKey(KeyCode.D)) {
-            rigidBody2d.velocity += Vector2.right * movementSpeed * Time.deltaTime;
-            harpoon.CancelHarpoon();
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveRight();
         }
+    }
+
+    private void moveLeft()
+    {
+        rigidBody2d.velocity += Vector2.left * movementSpeed * Time.deltaTime;
+        harpoon.CancelHarpoon();
+        spriteRenderer.flipX = true;
+    }
+
+    private void moveRight() {
+        rigidBody2d.velocity += Vector2.right * movementSpeed * Time.deltaTime;
+        harpoon.CancelHarpoon();
+        spriteRenderer.flipX = false;
     }
 }
